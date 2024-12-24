2ad0fea277635a778ec21ed8f7daadf90c020db4
BIRSAx2 <hello@mouhieddine.dev>
2024-12-24 23:29:13 +0100

    2020/3

--- 2020/lib/day2.ml
+++ 2020/lib/day2.ml
@@ -1,24 +1,46 @@
 open! Imports
+open! Base
 
 module M = struct
-  (* Type to parse the input into *)
-  type t = unit
+  type t = (Base.int * Base.int * string * string) Base.list
 
-  (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse inputs =
+    inputs
+    |> String.split_on_chars ~on:['\n']
+    |> List.map ~f:(fun line ->
+           let re = Re2.create_exn "(\\d+)-(\\d+) (\\w): (\\w+)" in
+           let matches = Re2.find_submatches_exn re line in
+           let minimum = Int.of_string (Option.value_exn matches.(1)) in
+           let maximum = Int.of_string (Option.value_exn matches.(2)) in
+           let ch = Option.value_exn matches.(3) in
+           let pw = Option.value_exn matches.(4) in
+           (minimum, maximum, ch, pw) )
 
-  (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let is_valid_password (min, max, char, pwd) =
+    let count = String.count pwd ~f:(fun c -> Char.equal c char.[0]) in
+    count >= min && count <= max
 
-  (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let part1 input =
+    input
+    |> List.filter ~f:is_valid_password
+    |> List.length |> Stdio.printf "%d\n"
+
+  let is_valid_password_with_position (min, max, char, pwd) =
+    let valid_position_1 = Char.equal (String.get pwd (min - 1)) char.[0] in
+    let valid_position_2 = Char.equal (String.get pwd (max - 1)) char.[0] in
+    Bool.(valid_position_1 <> valid_position_2)
+
+  let part2 inputs =
+    inputs
+    |> List.filter ~f:is_valid_password_with_position
+    |> List.length |> Stdio.printf "%d\n"
 end
 
 include M
 include Day.Make (M)
 
-(* Example input *)
-let example = ""
+let example = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
 
-(* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {|
+    2
+    1 |}]
--- 2020/lib/day3.ml
+++ 2020/lib/day3.ml
@@ -1,24 +1,81 @@
 open! Imports
+open! Base
 
 module M = struct
-  (* Type to parse the input into *)
-  type t = unit
+  type t = (int * int, int) Base.Hashtbl.t * (int * int)
 
-  (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let parse (args : string) : t =
+    let lines =
+      String.split ~on:'\n' args
+      |> List.filter ~f:(fun s -> not (String.is_empty s))
+    in
+    let tree_map = Hashtbl.Poly.create ~size:32 () in
+    List.iteri lines ~f:(fun y line ->
+        let chars = String.to_list line in
+        List.iteri chars ~f:(fun x c ->
+            let data = if Char.equal c '#' then 1 else 0 in
+            Hashtbl.set tree_map ~key:(x, y) ~data ) ) ;
+    let compare_coord (x1, y1) (x2, y2) =
+      match Int.compare x1 x2 with 0 -> Int.compare y1 y2 | c -> c
+    in
+    let bottom_right =
+      Hashtbl.fold tree_map ~init:(0, 0)
+        ~f:(fun ~key:(x, y) ~data:_ (best_x, best_y) ->
+          if compare_coord (x, y) (best_x, best_y) > 0 then (x, y)
+          else (best_x, best_y) )
+    in
+    (tree_map, bottom_right)
 
-  (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let is_tree ((tree_map, (max_x, _max_y)) : t) (x, y) : int =
+    let x_mod = x % (max_x + 1) in
+    Hashtbl.find tree_map (x_mod, y) |> Option.value ~default:0
 
-  (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let traverse_count_tree ((_, (_max_x, max_y)) as t) (slope_x, slope_y) :
+      int =
+    let range = List.range 0 max_y in
+    let final_count, _ =
+      List.fold range
+        ~init:(0, (slope_x, slope_y))
+        ~f:(fun (count, (x, y)) _ ->
+          let count = count + is_tree t (x, y) in
+          let x' = x + slope_x in
+          let y' = y + slope_y in
+          (count, (x', y')) )
+    in
+    final_count
+
+  let part1 input =
+    let trees_hit = traverse_count_tree input (3, 1) in
+    Stdio.printf "%d\n" trees_hit
+
+  (* part2: parse input, then multiply results from several slopes. *)
+  let part2 input =
+    let slopes = [(1, 1); (3, 1); (5, 1); (7, 1); (1, 2)] in
+    let product =
+      List.map slopes ~f:(traverse_count_tree input)
+      |> List.fold ~init:1 ~f:( * )
+    in
+    Stdio.printf "%d\n" product
 end
 
 include M
 include Day.Make (M)
 
-(* Example input *)
-let example = ""
+let example =
+  "..##.......\n\
+   #...#...#..\n\
+   .#....#..#.\n\
+   ..#.#...#.#\n\
+   .#...##..#.\n\
+   ..#.##.....\n\
+   .#.#.#....#\n\
+   .#........#\n\
+   #.##...#...\n\
+   #...##....#\n\
+   .#..#...#.#"
 
 (* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {|
+7
+336
+|}]
