4f583ff33dfa07c917f6ae20dfd9b32c03687a33
BIRSAx2 <hello@mouhieddine.dev>
2024-12-24 22:46:34 +0100

    2020/1

--- 2020/lib/day1.ml
+++ 2020/lib/day1.ml
@@ -1,24 +1,45 @@
 open! Imports
+open! Base
 
 module M = struct
-  (* Type to parse the input into *)
-  type t = unit
+  type t = Set.M(Int).t
 
-  (* Parse the input to type t, invoked for both parts *)
-  let parse _inputs = ()
+  let find_sum numbers target =
+    Set.to_sequence numbers
+    |> Sequence.find ~f:(fun x -> Set.mem numbers (target - x))
+    |> Option.map ~f:(fun x -> (x, target - x))
 
-  (* Run part 1 with parsed inputs *)
-  let part1 _ = ()
+  let find_three_sum numbers target =
+    Set.to_sequence numbers
+    |> Sequence.find_map ~f:(fun a ->
+           match find_sum numbers (target - a) with
+           | None -> None
+           | Some (b, c) -> Some (a, b, c) )
 
-  (* Run part 2 with parsed inputs *)
-  let part2 _ = ()
+  let parse inputs =
+    inputs
+    |> String.split_on_chars ~on:['\n']
+    (* Remove empty lines if any. *)
+    |> List.filter ~f:(fun s -> not (String.is_empty s))
+    |> List.map ~f:Int.of_string
+    |> Set.of_list (module Int)
+
+  let part1 numbers =
+    match find_sum numbers 2020 with
+    | None -> Stdio.print_endline "0"
+    | Some (a, b) -> Stdio.printf "%d\n" (a * b)
+
+  let part2 numbers =
+    match find_three_sum numbers 2020 with
+    | None -> Stdio.print_endline "0"
+    | Some (a, b, c) -> Stdio.printf "%d\n" (a * b * c)
 end
 
 include M
 include Day.Make (M)
 
-(* Example input *)
-let example = ""
+let example = "1721\n979\n366\n299\n675\n1456"
 
-(* Expect test for example input *)
-let%expect_test _ = run example ; [%expect {| |}]
+let%expect_test _ = run example ; [%expect {|
+    514579
+    241861950 |}]
